77c74933c68305fd4a60af26fe14d644cbfc21f9
Zaeler-wut <150231365+Zaeler-wut@users.noreply.github.com>
2025-08-28 14:12:53 +0700

    add 3-1,3-2

---
+++ week3/3-1.js
@@ -0,0 +1,20 @@
+let score = 61;
+if (score >= 80 && score <= 100) {
+  console.log("A");
+}
+
+if (score >= 60 && score <= 69) {
+  console.log("B");
+}
+
+if (score >= 70 && score <= 79) {
+  console.log("B");
+}
+
+if (score >= 50 && score <= 59) {
+  console.log("B");
+}
+
+if (score < 50) {
+  console.log("F");
+}
---
+++ week3/3-2.js
@@ -0,0 +1,14 @@
+let productPrice = 1000;
+let discount = 10;
+
+let discountPrice = productPrice-((productPrice*discount)/100);
+
+if (discountPrice <= 0) {
+    console.log("0")
+}else if (discountPrice <= 500) {
+    discountPrice += 50
+    console.log(discountPrice)
+}else if (discountPrice >= 500) {
+    discountPrice *= 0.9
+    console.log(discountPrice)
+}
